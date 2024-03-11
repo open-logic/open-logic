5acae610b5d64859fd7275f841b8ab14a5b42899
Oliver Bründler <oli.bruendler@gmx.ch>
2024-03-11 22:31:59 +0100

    Update GCS key installation

--- .github/workflows/install_gcs_key.sh
+++ .github/workflows/install_gcs_key.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #Run Unpack secret
+sudo apt-get update
+sudo apt-get install openssl
 openssl enc -k $GCS_PASSPHRASE -d -aes-256-cbc -in ./.github/workflows/open-logic-badges.json.gpg -out ~/gcs.json 
 export GCS_FILE=~/gcs.json
