d01db17a42c1b994205d60a0dff48a2d314f3d2b
Oliver Bründler <oli.bruendler@gmx.ch>
2024-03-11 22:33:10 +0100

    Fix key file

--- .github/workflows/install_gcs_key.sh
+++ .github/workflows/install_gcs_key.sh
@@ -3,5 +3,5 @@
 #Run Unpack secret
 sudo apt-get update
 sudo apt-get install openssl
-openssl enc -k $GCS_PASSPHRASE -d -aes-256-cbc -in ./.github/workflows/open-logic-badges.json.gpg -out ~/gcs.json 
+openssl enc -k $GCS_PASSPHRASE -d -aes-256-cbc -in ./.github/workflows/open-logic-badges.enc -out ~/gcs.json 
 export GCS_FILE=~/gcs.json
