69d43a34c86b5b4da6fe33dd9f1245797ac9e2a2
Oliver Bründler <oli.bruendler@gmx.ch>
2024-03-11 22:18:09 +0100

    Update key install script

--- .github/workflows/install_gcs_key.sh
+++ .github/workflows/install_gcs_key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Run Unpack secret
-gpg --quiet --batch --yes --decrypt --passphrase=$GCS_PASSPHRASE --output ~/gcs.json ./.github/workflows/open-logic-badges.json.gpg
+gpg -v --no-use-agent --batch --yes --decrypt --passphrase=$GCS_PASSPHRASE --output ~/gcs.json ./.github/workflows/open-logic-badges.json.gpg
 export GCS_FILE=~/gcs.json
