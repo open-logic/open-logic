f1dd068989699783bdd7b139c0b0e64add6cad58
Oliver Bründler <oli.bruendler@gmx.ch>
2024-06-27 00:00:55 +0200

    BUGFIX: Fix scoped constraints script
    Avoid warnings if an entity is not included
    Use namespaces to avoid unwanted interactions between scripts

--- src/base/tcl/constraints_amd.tcl
+++ src/base/tcl/constraints_amd.tcl
@@ -1,10 +1,12 @@
 #Automatically load all constraints
+namespace eval olo_base_constraints_amd {
 
-variable fileLoc [file normalize [file dirname [info script]]]
+	variable fileLoc [file normalize [file dirname [info script]]]
 
-puts "OLO LOAD read_xdc for base"
+	puts "OLO LOAD read_xdc for base"
 
-read_xdc -ref olo_base_cc_reset $fileLoc/olo_base_cc_reset.tcl
-read_xdc -ref olo_base_cc_bits $fileLoc/olo_base_cc_bits.tcl
-read_xdc -ref olo_base_cc_simple $fileLoc/olo_base_cc_simple.tcl
+	read_xdc -quiet -ref olo_base_cc_reset $fileLoc/olo_base_cc_reset.tcl
+	read_xdc -quiet -ref olo_base_cc_bits $fileLoc/olo_base_cc_bits.tcl
+	read_xdc -quiet -ref olo_base_cc_simple $fileLoc/olo_base_cc_simple.tcl
+}
 
--- src/intf/tcl/constraints_amd.tcl
+++ src/intf/tcl/constraints_amd.tcl
@@ -1,8 +1,9 @@
 #Automatically load all constraints
+namespace eval olo_intf_constraints_amd {
+	variable fileLoc [file normalize [file dirname [info script]]]
 
-variable fileLoc [file normalize [file dirname [info script]]]
+	puts "OLO LOAD read_xdc for intf"
 
-puts "OLO LOAD read_xdc for intf"
-
-read_xdc -ref olo_intf_sync $fileLoc/olo_intf_sync.tcl
+	read_xdc -quiet -ref olo_intf_sync $fileLoc/olo_intf_sync.tcl
+}
 
