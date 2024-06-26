e059185e37ee1881d5759124e91c0b0056083825
Oliver Bründler <oli.bruendler@gmx.ch>
2024-06-26 14:15:33 +0200

    FEATURE: Add scoped constraints for olo_intf_i2c_master
    Only for the case where tristate is internal (otherwise the signals are used inside the FPGA)

--- src/intf/tcl/constraints_amd.tcl
+++ src/intf/tcl/constraints_amd.tcl
@@ -5,4 +5,6 @@ variable fileLoc [file normalize [file dirname [info script]]]
 puts "OLO LOAD read_xdc for intf"
 
 read_xdc -ref olo_intf_sync $fileLoc/olo_intf_sync.tcl
+read_xdc -ref olo_intf_i2c_master $fileLoc/olo_intf_i2c_master.tcl
+
 
