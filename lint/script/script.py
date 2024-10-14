6c81d41fc5710a73da0fd7b094e1668937fd038c
Oliver Bründler <oli.bruendler@gmx.ch>
2024-10-14 11:40:59 +0200

    VS Code integration

--- lint/script/script.py
+++ lint/script/script.py
@@ -7,6 +7,7 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Detect arguments
 parser = argparse.ArgumentParser(description='Lint all VHDL files in the project')
 parser.add_argument('--debug', action='store_true', help='Lint files one by one and stop on any errors')
+parser.add_argument('--syntastic', action='store_true', help='Output in syntastic format')
 
 args = parser.parse_args()
 
@@ -33,7 +34,7 @@ def find_normal_vhd_files(directory):
             if file in NOT_LINTED:
                 continue
             #Append file
-            vhd_files.append(os.path.join(root, file))
+            vhd_files.append(os.path.abspath(os.path.join(root, file)))
     return vhd_files
 
 def find_vc_vhd_files(directory):
@@ -42,8 +43,14 @@ def find_vc_vhd_files(directory):
         for file in files:
             # Skip VC files
             if file.endswith('_vc.vhd'):
-                vhd_files.append(os.path.join(root, file))
+                vhd_files.append(os.path.abspath(os.path.join(root, file)))
     return vhd_files
+
+# Configure output format
+otutput_format = "-of vsg"
+if args.syntastic:
+    otutput_format = "-of syntastic"
+
 
 # Get the list of .vhd files
 vhd_files_list = find_normal_vhd_files(DIR)
@@ -55,12 +62,12 @@ error_occurred = False
 if args.debug:
     for file in vhd_files_list:
         print(f"Linting {file}")
-        result = os.system(f'vsg -c ../config/vsg_config.yml -f {file}')
+        result = os.system(f'vsg -c ../config/vsg_config.yml -f {file} {otutput_format}')
         if result != 0:
             raise Exception(f"Error: Linting of {file} failed - check report")
 else:
     all_files = " ".join(vhd_files_list)
-    result = os.system(f'vsg -c ../config/vsg_config.yml -f {all_files} --junit ../report/vsg_normal_vhdl.xml --all_phases')
+    result = os.system(f'vsg -c ../config/vsg_config.yml -f {all_files} --junit ../report/vsg_normal_vhdl.xml --all_phases {otutput_format}')
     if result != 0:
         error_occurred = True
 
@@ -68,12 +75,12 @@ else:
 if args.debug:
     for file in vc_files_list:
         print(f"Linting {file}")
-        result = os.system(f'vsg -c ../config/vsg_config.yml ../config/vsg_config_overlay_vc.yml -f {file}')
+        result = os.system(f'vsg -c ../config/vsg_config.yml ../config/vsg_config_overlay_vc.yml -f {file} {otutput_format}')
         if result != 0:
             raise Exception(f"Error: Linting of {file} failed - check report")
 else:
     all_files = " ".join(vc_files_list) 
-    result = os.system(f'vsg -c ../config/vsg_config.yml ../config/vsg_config_overlay_vc.yml  -f {all_files} --junit ../report/vsg_vc_vhdl.xml --all_phases')
+    result = os.system(f'vsg -c ../config/vsg_config.yml ../config/vsg_config_overlay_vc.yml  -f {all_files} --junit ../report/vsg_vc_vhdl.xml --all_phases {otutput_format}')
     if result != 0:
         error_occurred = True
 
