6ae428bef1dd0e020f73082eb1332de00063d6b6
Oliver Bründler <oli.bruendler@gmx.ch>
2024-10-14 12:02:16 +0200

    Fix comments in RBExample

--- doc/axi/slave/RbExample.vhd
+++ doc/axi/slave/RbExample.vhd
@@ -1,25 +1,25 @@
 p_rb : process(Clk)
 begin
     if rising_edge(Clk) then
--- Write
+        -- Write
         if Rb_Wr = '1' then
             case Rb_Addr is
                 when X"00" =>
--- Register with byte enables
+                    -- Register with byte enables
                     for i in 0 to 3 loop
                         SomeReg(8*(i+1)-1 downto 8*i) <= Rb_WrData(8*i-1 downto 8*i);
                     end loop;$
                 when X"04" =>
--- Register without byte enables
+                    -- Register without byte enables
                     OtherReg <= Rb_WrData;
                 when X"08" =>
--- Register with clear-by-write-one bits
+                    -- Register with clear-by-write-one bits
                     VectorReg <= VectorReg and not Rb_WrData;
                 when others => null;
             end case;
         end if;
 
--- Read
+        -- Read
         Rb_RdValid <= '0'; -- Defuault value
         if Rb_Rd = '1' then
             case Rb_Addr is
@@ -27,7 +27,7 @@ begin
                     Rb_RdData <= SomeReg;
                     Rb_RdValid <= '1';
                 when X"04" =>
--- Register with clear-on-write
+                    -- Register with clear-on-write
                     Rb_RdData <= OtherReg;
                     OtherReg <= (others => '0');
                     Rb_RdValid <= '1';
@@ -38,6 +38,6 @@ begin
             end case;
         end if;
 
--- Reset and other logic omitted
+        -- Reset and other logic omitted
     end if;
 end process;
