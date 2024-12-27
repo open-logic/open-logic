1e62e081a3e123d400ad851bd9d1966f9a1a59d2
Oliver Bründler <oli.bruendler@gmx.ch>
2024-12-27 13:41:30 +0100

    Implement scripted generation of -dev core-files

--- tools/fusesoc/UpdateCoreFiles.py
+++ tools/fusesoc/UpdateCoreFiles.py
@@ -41,51 +41,81 @@ os.chdir("../../src")
 # Get all subdirectories
 areas = os.listdir()
 
-# Iterate over all areas
-for area in areas:
-    # Navigate to area
-    os.chdir(area)
-    # Get all VHDL files
-    os.chdir("vhdl")
-    vhdlFiles = os.listdir()
-    # Navigate to area
-    os.chdir("..")
+# Generate dev/stable cores
+for state in ["dev", "stable"]:
 
-    # Generaete core-file
-    template = env.get_template("core.template")
+    # Select library name and codebase
+    if state == "dev":
+        codebase = "local files (release plus WIP)"
+        postfix = "-dev"
+        filepostfix = "_dev"
+    elif state == "stable":
+        codebase = "official release (stable)"
+        postfix = ""
+        filepostfix = ""
+    else:
+        raise ValueError("Invalid state (dev/stable)")
+
+    library = "open-logic" + postfix
+    tutorial_library = "tutorials" + postfix
+
+    # Iterate over all areas
+    for area in areas:    
+
+        # Navigate to area
+        os.chdir(area)
+        # Get all VHDL files
+        os.chdir("vhdl")
+        vhdlFiles = os.listdir()
+        # Navigate to area
+        os.chdir("..")
+
+        # Generaete core-file
+        template = env.get_template("core.template")
+        data = {
+            "area" : area,
+            "vhdlFiles" : vhdlFiles,
+            "version" : VERSION,
+            "description" : DESCRIPTIONS[area],
+            "dependencies" : DEPENDENCIES[area],
+            "library" : library,
+            "codebase" : codebase
+        }
+        rendered_template = template.render(data)
+        with open(f"olo_{area}{filepostfix}.core", "w+") as f:
+            f.write(rendered_template)
+
+
+        # Navigate to src
+        os.chdir("..")
+
+    #Tutorials
+    os.chdir("../doc/tutorials")
     data = {
-        "area" : area,
-        "vhdlFiles" : vhdlFiles,
-        "version" : VERSION,
-        "description" : DESCRIPTIONS[area],
-        "dependencies" : DEPENDENCIES[area]
+        "version": VERSION,
+        "library": library,
+        "codebase": codebase,
+        "tutorial_library": tutorial_library
     }
+    # Vivado Tutorial
+    os.chdir("VivadoTutorial/Files")
+    template = env.get_template("olo_vivado_tutorial.template")
     rendered_template = template.render(data)
-    with open(f"olo_{area}.core", "w+") as f:
+    with open(f"olo_vivado_tutorial{filepostfix}.core", "w+") as f:
+        f.write(rendered_template)
+    os.chdir("../..")
+
+    #Quartus Tutorial
+    os.chdir("QuartusTutorial/Files")
+    template = env.get_template("olo_quartus_tutorial.template")
+    rendered_template = template.render(data)
+    with open(f"olo_quartus_tutorial{filepostfix}.core", "w+") as f:
         f.write(rendered_template)
 
+    # Navigate back to src
+    os.chdir("../../../../src")
 
-    # Navigate to src
-    os.chdir("..")
 
-#Tutorials
-os.chdir("../doc/tutorials")
-data = {
-    "version": VERSION
-}
-# Vivado Tutorial
-os.chdir("VivadoTutorial/Files")
-template = env.get_template("olo_vivado_tutorial.template")
-rendered_template = template.render(data)
-with open(f"olo_vivado_tutorial.core", "w+") as f:
-    f.write(rendered_template)
-os.chdir("../..")
-#Quartus Tutorial
-os.chdir("QuartusTutorial/Files")
-template = env.get_template("olo_quartus_tutorial.template")
-rendered_template = template.render(data)
-with open(f"olo_quartus_tutorial.core", "w+") as f:
-    f.write(rendered_template)
 # Navigate back to tools
 os.chdir(curdir)
 
