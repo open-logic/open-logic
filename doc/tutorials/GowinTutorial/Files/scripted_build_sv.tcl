c12f5e963aa32b8b2ff7662e4b8a9192a120e4d3
Oliver Bründler <oli.bruendler@gmx.ch>
2025-01-08 22:25:52 +0100

    Implement Gowin Tutorial

---
+++ doc/tutorials/GowinTutorial/Files/scripted_build_sv.tcl
@@ -0,0 +1,25 @@
+#Create Project
+set oloRoot [file normalize "[file dirname [info script]]/../../../.."]
+create_project -name tutorial_prj_sv -dir . -pn GW1N-LV4LQ144C6/I5 -device_version D -force
+after 5000
+open_project ./tutorial_prj_sv/tutorial_prj_sv.gprj
+set_option -synthesis_tool gowinsynthesis
+set_option -output_base_name gowin_tutorial
+set_option -vhdl_std vhd2008
+set_option -verilog_std sysv2017
+set_option -print_all_synthesis_warning 1
+
+#Add OLO
+source $oloRoot/tools/gowin/import_sources.tcl
+
+#Add Files
+add_file -type verilog "$oloRoot/doc/tutorials/GowinTutorial/Files/gowin_tutorial.sv"
+add_file -type cst "$oloRoot/doc/tutorials/GowinTutorial/Files/gowin_tutorial.cst"
+add_file -type sdc "$oloRoot/doc/tutorials/GowinTutorial/Files/constraints.sdc"
+
+#Build
+set_option -top_module gowin_tutorial
+run all
+
+puts "Done"
+
