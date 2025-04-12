adf6ff9d1e44eb9992d700862b3d1e67481edee4
Oliver Bründler <oli.bruendler@gmx.ch>
2025-04-12 22:38:27 +0200

    Add first python models

---
+++ src/fix/python/olo_fix_abs.py
@@ -0,0 +1,68 @@
+# ---------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 by Oliver Bründler
+# All rights reserved.
+# Authors: Oliver Bruendler
+# ---------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------------------------------
+
+# Import en_cl_fix
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../3rdParty/en_cl_fix/bittrue/models/python")))
+
+# Import the necessary modules
+from en_cl_fix_pkg import *
+
+# ---------------------------------------------------------------------------------------------------
+# Class
+# ---------------------------------------------------------------------------------------------------
+class olo_fix_abs:
+    """
+    Model of olo_fix_abs entity
+    """
+
+    # ---------------------------------------------------------------------------------------------------
+    # Constructor
+    # ---------------------------------------------------------------------------------------------------
+    def __init__(self,
+                 a_fmt : FixFormat,
+                 result_fmt : FixFormat,
+                 round : FixRound = FixRound.Trunc_s,
+                 saturate : FixSaturate = FixSaturate.Warn_s):
+        """
+        Constructor for the olo_fix_abs class.
+        :param a_fmt: Format of the a input
+        :param result_fmt: Format of the result
+        :param round: Rounding mode
+        :param saturate: Saturation mode
+        """
+        self._a_fmt = a_fmt
+        self._result_fmt = result_fmt
+        self._round = round
+        self._saturate = saturate
+
+    def reset(self):
+        """
+        Reset state of the component
+        """
+        pass #Does not have state
+
+    def next(self, a):
+        """
+        Process next N samples
+        :param a: Input a
+        :return: Processed result
+        """
+        return cl_fix_abs(a, self._a_fmt, self._result_fmt, self._round, self._saturate)
+
+    def process(self, a):
+        """
+        Process samples (without preserving previous state)
+        :param a: Input a
+        :return: Processed result
+        """
+        self.reset()
+        return self.next(a)
---
+++ src/fix/python/tests/test_olo_fix_abs.py
@@ -0,0 +1,54 @@
+# ---------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 by Oliver Bründler
+# All rights reserved.
+# Authors: Oliver Bruendler
+# ---------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------------------------------
+
+# Import en_cl_fix
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from olo_fix_abs import olo_fix_abs
+from en_cl_fix_pkg import *
+
+# Note: Test coverage is OK for the Open Logic code, it does not cover all numerics because
+#       this is covered by en_cl_fix_pkg tests already.
+class TestOloFixAbs(unittest.TestCase):
+
+    def setUp(self):
+        self.abs = olo_fix_abs(a_fmt=FixFormat(1, 8, 8), result_fmt=FixFormat(1, 8, 8))
+
+    def test_scalar(self):
+        self.assertEqual(self.abs.process(-5.5), 5.5)
+        self.assertEqual(self.abs.process(5.5), 5.5)
+
+    def test_array(self):
+        a = [-5.5, 3.25, -1.0, 0.0, 4.5]
+        expected = [5.5, 3.25, 1.0, 0.0, 4.5]
+        result = self.abs.process(a)
+        self.assertListEqual(list(result), list(expected))
+
+    def test_next_scalar(self):
+        a = [-5.5, 3.25, -1.0, 0.0, 4.5]
+        expected = [5.5, 3.25, 1.0, 0.0, 4.5]
+        result = []
+        for sample in a:
+            result.append(self.abs.next(sample))
+        self.assertListEqual(result, expected)
+
+    def test_next_list(self):
+        a = [-5.5, 3.25, -1.0, 0.0, 4.5]
+        expected = [5.5, 3.25, 1.0, 0.0, 4.5]
+        result = []
+        result.extend(self.abs.next(a[:2]))
+        result.extend(self.abs.next(a[2:]))
+        self.assertListEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
