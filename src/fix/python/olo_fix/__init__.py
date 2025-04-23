6a6381a9596f8fdecd2ed542ca0c5a305e504506
Oliver Bründler <oli.bruendler@gmx.ch>
2025-04-23 20:14:34 +0200

    Check-in Pycharm workspace

--- src/fix/python/olo_fix/__init__.py
+++ src/fix/python/olo_fix/__init__.py
@@ -1,14 +1,14 @@
-import sys
-import os
-import glob
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../3rdParty/en_cl_fix/bittrue/models/python")))
-
-
-# Dynamically import all modules in the current directory
-# Allos importing modules as "from olo_fix import olo_fix_sub" instead of "from olo_fix.olo_fix_sub import olo_fix_sub"
-module_dir = os.path.dirname(__file__)
-module_files = glob.glob(os.path.join(module_dir, "*.py"))
-module_names = [os.path.basename(f)[:-3] for f in module_files if os.path.isfile(f) and not f.endswith("__init__.py")]
-
-for module_name in module_names:
-    exec(f"from .{module_name} import {module_name}")
+from olo_fix_abs import *
+from olo_fix_add import *
+from olo_fix_addsub import *
+from olo_fix_compare import *
+from olo_fix_cosim import *
+from olo_fix_from_real import *
+from olo_fix_mult import *
+from olo_fix_neg import *
+from olo_fix_plots import *
+from olo_fix_resize import *
+from olo_fix_round import *
+from olo_fix_saturate import *
+from olo_fix_sub import *
+from olo_fix_utils import *
