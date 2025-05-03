20129b2a08ba870523c81cd729ba1daefb12bd9f
Rene Brglez <rene.brglez@aviatnet.com>
2025-05-03 18:15:08 +0200

    WIP

--- src/intf/vhdl/olo_intf_pwm.vhd
+++ src/intf/vhdl/olo_intf_pwm.vhd
@@ -24,13 +24,18 @@ entity olo_intf_pwm is
         Clk : in std_logic;
         Rst : in std_logic;
 
-        In_En     : in std_logic                                            := '1';
-        In_Period : in std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0) := toUslv(MaxPeriod_g, log2Ceil(MaxPeriod_g+1));
-        In_OnTime : in std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0);
+        In_En : in std_logic := '1';
 
-        Out_PeriodStart : out std_logic;
-        Out_PeriodCnt   : out std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0);
-        Out_Pwm         : out std_logic
+        In_Valid  : in  std_logic;
+        In_Ready  : out std_logic;
+        In_Period : in  std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0) := toUslv(MaxPeriod_g, log2Ceil(MaxPeriod_g+1));
+        In_OnTime : in  std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0);
+
+        Out_Period : out std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0);
+        Out_OnTime : out std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0);
+
+        Out_PeriodCnt : out std_logic_vector(log2Ceil(MaxPeriod_g+1)-1 downto 0);
+        Out_Pwm       : out std_logic
     );
 end entity;
 
@@ -41,12 +46,22 @@ architecture rtl of olo_intf_pwm is
 
     -- *** Two Process Method ***
     type TwoProcess_r is record
-        Period : unsigned(In_Period'range);
-        OnTime : unsigned(In_OnTime'range);
+        Ready_r1 : std_logic;
+
+        ValuesLatched_r1 : std_logic;
+        PeriodLatched_r1 : unsigned(In_Period'range);
+        OnTimeLatched_r1 : unsigned(In_OnTime'range);
+
+        ValuesActive_r2 : std_logic;
+        PeriodActive_r2 : unsigned(In_Period'range);
+        PeriodActive_r3 : unsigned(In_Period'range);
+        OnTimeActive_r2 : unsigned(In_OnTime'range);
+        OnTimeActive_r3 : unsigned(In_OnTime'range);
+
         --
-        PeriodStart : std_logic;
-        PeriodCnt   : unsigned(Out_PeriodCnt'range);
-        Pwm         : std_logic;
+        PeriodCnt_r2 : unsigned(Out_PeriodCnt'range);
+        PeriodCnt_r1 : unsigned(Out_PeriodCnt'range);
+        Pwm          : std_logic;
     end record;
 
     signal r      : TwoProcess_r;
@@ -62,32 +77,64 @@ begin
         v := r;
 
         -- *** Default Values ***
-        v.PeriodStart := '0';
 
-        if (v.PeriodCnt = to_unsigned(0, r.PeriodCnt'length)) then
+        ------------------------------------------------------------------------
+        -- R0
+        ------------------------------------------------------------------------
+        if (In_Valid = '1' and r.Ready_r1 = '1') then
             -- Update values for Period and OnTime
-            v.Period := unsigned(In_Period);
-            v.OnTime := unsigned(In_OnTime);
+            v.PeriodLatched_r1 := unsigned(In_Period);
+            v.OnTimeLatched_r1 := unsigned(In_OnTime);
+
+            v.Ready_r1 := '0';
+
+            v.ValuesLatched_r1 := '1';
         end if;
 
-        if (In_En = '1') then
-            if (v.Period = to_unsigned(0, r.Period'length)) then
-                -- Don't Increment PeriodCnt if Period = 0
-                v.PeriodCnt := (others => '0');
+        ------------------------------------------------------------------------
+        -- R1
+        ------------------------------------------------------------------------
+        if (r.ValuesLatched_r1 = '1') then
+            if (r.PeriodCnt_r2 = to_unsigned(0, In_Period'length)) then
+                v.PeriodActive_r2 := r.PeriodLatched_r1;
+                v.OnTimeActive_r2 := r.OnTimeLatched_r1;
 
-            elsif (r.PeriodCnt < r.Period - 1) then
-                -- Increment Period Counter
-                v.PeriodCnt := v.PeriodCnt + 1;
+                v.ValuesActive_r2  := '1';
+                v.ValuesLatched_r1 := '0';
+            end if;
+        end if;
+
+
+        if (r.ValuesActive_r2 = '1') then
+            if (r.PeriodActive_r2 = to_unsigned(0, In_Period'length)) then
+                v.Ready_r1     := '1';
+                v.PeriodCnt_r1 := (others => '0');
+            elsif (r.PeriodCnt_r1 < r.PeriodActive_r2 - 1) then
+                v.PeriodCnt_r1 := r.PeriodCnt_r1 + 1;
+
             else
-                -- Reset Period Counter
-                v.PeriodStart := '1';
-                v.PeriodCnt   := (others => '0');
+                v.Ready_r1     := '1';
+                v.PeriodCnt_r1 := (others => '0');
             end if;
 
-        else
-            -- Reset Period Counter if In_En isn't set
-            v.PeriodCnt := (others => '0');
         end if;
+
+
+        ------------------------------------------------------------------------
+        -- R2
+        ------------------------------------------------------------------------
+
+        if (r.PeriodCnt_r1 < r.OnTimeActive_r2) then
+            v.Pwm := '1';
+        else
+            v.Pwm := '0';
+        end if;
+
+
+        v.PeriodCnt_r2    := r.PeriodCnt_r1;
+        v.PeriodActive_r3 := r.PeriodActive_r2;
+        v.OnTimeActive_r3 := r.OnTimeActive_r2;
+
 
         -- Apply to record
         r_next <= v;
@@ -95,11 +142,14 @@ begin
     end process;
 
     -- Outputs
-    Out_PeriodStart <= '1' when (r.PeriodCnt = 0) and (In_En = '1') else '0';
-    Out_PeriodCnt   <= std_logic_vector(r.PeriodCnt);
-    Out_Pwm         <=
-        '1' when (r.PeriodCnt < r.OnTime) and (r_next.OnTime > 0) and (In_En = '1') else
-        '0';
+    In_Ready <= r.Ready_r1;
+
+    Out_Period <= std_logic_vector(r.PeriodActive_r3);
+    Out_OnTime <= std_logic_vector(r.OnTimeActive_r3);
+
+    Out_PeriodCnt <= std_logic_vector(r.PeriodCnt_r2);
+    Out_Pwm       <= r.Pwm;
+
 
     -- *** Sequential Process ***
     p_seq : process (Clk) is
@@ -107,9 +157,12 @@ begin
         if rising_edge(Clk) then
             r <= r_next;
             if (Rst = '1') then
-                r.PeriodStart <= '0';
-                r.PeriodCnt   <= (others => '0');
-                r.Pwm         <= '0';
+                r.Ready_r1         <= '1';
+                r.ValuesLatched_r1 <= '0';
+                r.ValuesActive_r2  <= '0';
+                r.PeriodCnt_r2     <= (others => '0');
+                r.PeriodCnt_r1     <= (others => '0');
+                r.Pwm              <= '0';
             end if;
         end if;
     end process;
