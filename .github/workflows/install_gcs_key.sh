3fa081e02bdf633de72f4e6d0790ca8a4666c958
Oliver Bründler <oli.bruendler@gmx.ch>
2025-05-24 10:32:14 +0200

    CLEANUP: Clean github workflows folder (#144)

--- .github/workflows/install_gcs_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Run Unpack secret
-sudo apt-get update
-sudo apt-get install openssl
-openssl enc -k $GCS_PASSPHRASE -d -aes-256-cbc -in ./.github/workflows/open-logic-badges.enc -out ~/gcs.json 
-export GCS_FILE=~/gcs.json
