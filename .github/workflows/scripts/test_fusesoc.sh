0c3e6c4b99f6233d0402d90f8b827adf8a5576ce
Oliver Bründler <oli.bruendler@gmx.ch>
2025-05-25 01:28:58 +0200

    BUGFIX: Fix fusesoc test-script

--- .github/workflows/scripts/test_fusesoc.sh
+++ .github/workflows/scripts/test_fusesoc.sh
@@ -12,20 +12,39 @@ echo "Open-Logic Root: $OLO_ROOT"
 # Info
 echo "Build Version $1"
 
-# Set-up fusesoc
-mkdir fusesoc
-cd fusesoc
-fusesoc library add open-logic https://github.com/open-logic/open-logic
+### Test dev-version of Open-Logic
 
-# Build Vivado (release / dev)
-fusesoc run --tool vivado --target zybo_z7 open-logic:tutorials:vivado_tutorial:$1
+# Set-up fusesoc
+mkdir fusesoc_local
+cd fusesoc_local
+fusesoc library add open-logic $OLO_ROOT
+
+# Build Vivado
 fusesoc run --tool vivado --target zybo_z7 open-logic:tutorials-dev:vivado_tutorial:$1
 
-# Build olo-fix tutorial (vivado only)
-fusesoc run --tool vivado --target zybo_z7 open-logic:tutorials:olo_fix_tutorial:$1
+# Build olo-fix tutorial
 fusesoc run --tool vivado --target zybo_z7 open-logic:tutorials-dev:olo_fix_tutorial:$1
 
-# Build Quartus (release / dev)
-fusesoc run --tool quartus --target de0_cv open-logic:tutorials:quartus_tutorial:$1
+# Build Quartus
 fusesoc run --tool quartus --target de0_cv open-logic:tutorials-dev:quartus_tutorial:$1
 
+# Return to original directory
+cd ..
+
+### Test Release of Open-Logic
+# Set-up fusesoc
+mkdir fusesoc_stable
+cd fusesoc_stable
+fusesoc library add open-logic https://github.com/open-logic/open-logic
+
+# Build Vivado
+fusesoc run --tool vivado --target zybo_z7 open-logic:tutorials:vivado_tutorial:$1
+
+# Build olo-fix tutorial
+fusesoc run --tool vivado --target zybo_z7 open-logic:tutorials:olo_fix_tutorial:$1
+
+# Build Quartus
+fusesoc run --tool quartus --target de0_cv open-logic:tutorials:quartus_tutorial:$1
+
+# Return to original directory
+cd ..
