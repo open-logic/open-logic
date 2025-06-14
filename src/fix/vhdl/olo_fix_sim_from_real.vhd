19ce09b14e1d233dfca519f3a8341de6afe44274
Oliver Bründler <oli.bruendler@gmx.ch>
2025-06-14 22:38:34 +0200

    NONCOMPATIBLE CHANGE: Removed real-port from olo_fix_from_real because not all tools accept it

--- src/fix/vhdl/olo_fix_sim_from_real.vhd
+++ src/fix/vhdl/olo_fix_sim_from_real.vhd
@@ -12,7 +12,7 @@
 -- hardware.
 --
 -- Documentation:
--- https://github.com/open-logic/open-logic/blob/main/doc/fix/olo_fix_from_real.md
+-- https://github.com/open-logic/open-logic/blob/main/doc/fix/olo_fix_sim_from_real.md
 --
 -- Note: The link points to the documentation of the latest release. If you
 --       use an older version, the documentation might not match the code.
@@ -33,7 +33,7 @@ library work;
 ---------------------------------------------------------------------------------------------------
 -- Entity Declaration
 ---------------------------------------------------------------------------------------------------
-entity olo_fix_from_real is
+entity olo_fix_sim_from_real is
     generic (
         -- Formats / Saturate
         ResultFmt_g : string;
@@ -42,12 +42,14 @@ entity olo_fix_from_real is
         Value_g     : real   := 0.0
     );
     port (
+        -- Input (only used of UsePort_g is true)
+        In_Value    : in    real := Value_g;
         -- Output
         Out_Value   : out   std_logic_vector(fixFmtWidthFromString(ResultFmt_g) - 1 downto 0)
     );
 end entity;
 
-architecture rtl of olo_fix_from_real is
+architecture rtl of olo_fix_sim_from_real is
 
     -- String to en_cl_fix
     constant ResultFmt_c : FixFormat_t   := cl_fix_format_from_string(ResultFmt_g);
@@ -56,6 +58,6 @@ architecture rtl of olo_fix_from_real is
 begin
 
     -- Convert real to fix
-    Out_Value <= cl_fix_from_real(Value_g, ResultFmt_c, Saturate_c);
+    Out_Value <= cl_fix_from_real(In_Value, ResultFmt_c, Saturate_c);
 
 end architecture;
