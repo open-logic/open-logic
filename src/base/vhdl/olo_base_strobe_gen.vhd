ef1fa1d00abf7cccf4912da1570797058101f1af
Oliver Bründler <oli.bruendler@gmx.ch>
2025-06-19 13:23:26 +0200

    Fix linter issue

--- src/base/vhdl/olo_base_strobe_gen.vhd
+++ src/base/vhdl/olo_base_strobe_gen.vhd
@@ -66,7 +66,6 @@ architecture rtl of olo_base_strobe_gen is
 
 begin
 
-
     -- Fractional mode is only lsupported for a factor < 1'000'000 between FreqClkHz_g and FreqStrobeHz_g
     assert not (FractionalMode_g and (FreqClkHz_g / FreqStrobeHz_g >= 1.0e6))
         report "olo_base_strobe_gen - Fractional mode is only supported for FreqClkHz_g < 1'000'000 x FreqStrobeHz_g"
