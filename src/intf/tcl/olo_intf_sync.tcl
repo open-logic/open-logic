4f8988f349b7568e4ad51fe1a1c8222938f9259e
Andrzej <59475345+AndrzejKowalski9917@users.noreply.github.com>
2026-05-11 22:10:32 +0200

    IMPROVEMENT: Make scoped constraints capable of handling mutli-clock driven cells e.g. through BUFGMUX (#308)
    contributed by @AndrzejKowalski9917

--- src/intf/tcl/olo_intf_sync.tcl
+++ src/intf/tcl/olo_intf_sync.tcl
@@ -5,7 +5,7 @@
 
 # Scoped constraints for olo_intf_sync
 # Load in vivado using "read_xdc -ref olo_intf_sync <path>/olo_intf_sync.tcl"
- 
+
 #Get D-Pins of Input FFs
 set in_ffs [get_pins -of_objects [get_cells *Reg0*] -filter {REF_PIN_NAME == D}]
 #Get Ports connected to those nets
@@ -14,7 +14,7 @@ set in_ports [get_ports -scoped_to_current_instance -prop_thru_buffers -of_objec
 set latch_clk [get_clocks -of_objects [get_cells *Reg0*]]
 
 #Set max delay to ensure delay Port->FF is less than one clock period
-set_max_delay -datapath_only [get_property PERIOD $latch_clk] -from $in_ports
+set_max_delay -datapath_only [get_property -min PERIOD $latch_clk] -from $in_ports
 
 #Make sure the first FF is not placed in the IOB. This ensures both FFs are placed in the same slice.
 set_property IOB FALSE [get_cells *Reg0*]
