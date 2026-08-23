c446d92fb51b8bc442d9fc02fa55793e898c1a4d
Rene Brglez <rene.brglez@aviatnet.com>
2026-08-23 12:15:16 +0200

    WIP: crc_append

--- src/base/vhdl/olo_base_crc_append.vhd
+++ src/base/vhdl/olo_base_crc_append.vhd
@@ -45,15 +45,17 @@ entity olo_base_crc_append is
         Clk              : in    std_logic;
         Rst              : in    std_logic;
         -- Input
-        In_Data          : in    std_logic_vector(DataWidth_g-1 downto 0);
-        In_Valid         : in    std_logic := '1';
         In_Ready         : out   std_logic;
+        In_Valid         : in    std_logic := '1';
         In_Last          : in    std_logic;
+        In_Be            : in    std_logic_vector(DataWidth_g/8-1 downto 0) := (others => '1');
+        In_Data          : in    std_logic_vector(DataWidth_g-1 downto 0);
         -- Output
-        Out_Data         : out   std_logic_vector(DataWidth_g-1 downto 0);
-        Out_Valid        : out   std_logic;
         Out_Ready        : in    std_logic := '1';
-        Out_Last         : out   std_logic
+        Out_Valid        : out   std_logic;
+        Out_Last         : out   std_logic;
+        Out_Be           : out   std_logic_vector(DataWidth_g/8 - 1 downto 0);
+        Out_Data         : out   std_logic_vector(DataWidth_g-1 downto 0)
     );
 end entity;
 
@@ -66,70 +68,219 @@ architecture rtl of olo_base_crc_append is
     -- *** Constants ***
     constant EntityName_c : string := "olo_base_crc_append";
 
+    constant CrcBytes_c : natural := CrcPolynomial_g'length/8;
+    constant BeWidth_c  : natural := DataWidth_g/8;
+    -- data + be + last
+    constant ConcatWidth_c : natural := DataWidth_g + BeWidth_c + 1;
+
     -- *** Types ***
     type State_t is (Data_s, Crc_s);
 
     -- *** Two Process Method ***
     type TwoProcess_r is record
-        State : State_t;
+        CrcByteCnt : natural;
+        State      : State_t;
     end record;
 
-    signal r, r_next : TwoProcess_r;
+    signal r      : TwoProcess_r;
+    signal r_next : TwoProcess_r;
 
     -- *** Instantiation Signals ***
+    signal In_Concat : std_logic_vector(ConcatWidth_c-1 downto 0);
+
     signal Crc_Valid : std_logic;
     signal Crc_Ready : std_logic;
     signal Crc_Crc   : std_logic_vector(CrcPolynomial_g'length-1 downto 0);
-    signal Pl_Valid  : std_logic;
-    signal Pl_Data   : std_logic_vector(DataWidth_g-1 downto 0);
-    signal Pl_Last   : std_logic;
-    signal Pl_Ready  : std_logic;
-    signal In_Beat   : std_logic;
+
+    signal PlIn_Valid  : std_logic;
+    signal PlIn_Last   : std_logic;
+    signal PlIn_Be     : std_logic_vector(BeWidth_c-1 downto 0);
+    signal PlIn_Data   : std_logic_vector(DataWidth_g-1 downto 0);
+    signal PlIn_Concat : std_logic_vector(ConcatWidth_c-1 downto 0);
+
+    signal Fsm_Ready  : std_logic;
+    signal Fsm_Valid  : std_logic;
+    signal Fsm_Last   : std_logic;
+    signal Fsm_Be     : std_logic_vector(BeWidth_c-1 downto 0);
+    signal Fsm_Data   : std_logic_vector(DataWidth_g-1 downto 0);
+    signal Fsm_Concat : std_logic_vector(ConcatWidth_c-1 downto 0);
+
+    signal PlOut_Valid  : std_logic;
+    signal PlOut_Last   : std_logic;
+    signal PlOut_Be     : std_logic_vector(BeWidth_c-1 downto 0);
+    signal PlOut_Data   : std_logic_vector(DataWidth_g-1 downto 0);
+    signal PlOut_Concat : std_logic_vector(ConcatWidth_c-1 downto 0);
 
 begin
 
     -- *** Assertions ***
-    assert CrcPolynomial_g'length <= DataWidth_g
-        report errorMessage(EntityName_c, "Polynomial_g must be smaller or equal width than DataWidth_g")
-        severity error;
+    -- TODO: Think about edge cases. What if CRC width is not multiple of 8 bits?
+
+    --  assert CrcPolynomial_g'length <= DataWidth_g
+    --      report errorMessage(EntityName_c, "Polynomial_g must be smaller or equal width than DataWidth_g")
+    --      severity error;
 
     -- *** Combinatorial Process ***
     p_comb : process (all) is
         variable v : TwoProcess_r;
+
+        -- Helper Variables
+        variable NumBe_v             : natural;
+        variable NumBeWithCrc_v      : natural;
+        variable CrcBytesRemaining_v : natural;
     begin
         -- *** Hold Variables Stable ***
         v := r;
 
         -- *** FSM ***
-        In_Ready  <= '0';
+        Fsm_Valid <= '0';
+        Fsm_Last  <= 'U';
+        Fsm_Be    <= (others => 'U');
+        Fsm_Data  <= (others => 'U');
+
         Crc_Ready <= '0';
-        Pl_Data   <= (others => 'X');
-        Pl_Valid  <= '0';
-        Pl_Last   <= '0';
-        In_Beat   <= '0';
 
         case r.State is
+            --------------------------------------------------------------------
             when Data_s =>
-                Pl_Data  <= In_Data;
-                Pl_Valid <= In_Valid;
-                In_Ready <= Pl_Ready;
-                In_Beat  <= In_Valid and Pl_Ready;
+                -- Reset CRC byte counter.
+                -- Normally, this counter is reset when the complete CRC has been 
+                -- appended to the stream. 
+                -- This redundant reset is kept as a safeguard in case the counter 
+                -- was not reset at the expected point.
+                v.CrcByteCnt := 0;
 
-                if In_Valid = '1' and Pl_Ready = '1' and In_Last = '1' then
-                    v.State := Crc_s;
+                if (PlIn_Valid = '1' and Fsm_Ready = '1') then
+
+                    -- Possible cases:
+                    --  1) Not the last data-beat
+                    --  2) Last data-beat
+
+                    -- 1) Not the last data-beat
+                    if (PlIn_Last = '0') then
+
+                        Fsm_Valid <= '1';
+                        Fsm_Last  <= '0';
+                        Fsm_Be    <= PlIn_Be;
+                        Fsm_Data  <= PlIn_Data;
+
+                    -- 2) Last data-beat
+                    elsif (PlIn_Last = '1') then
+                        
+                        -- Possible cases:
+                        -- 2.1) Last data-beat is fully filled with data
+                        -- 2.2) Last data-beat is paritally filled with data
+
+                        -- 2.1) Last data-beat is fully filled with data
+                        if (PlIn_Be = onesVector(BeWidth_c)) then
+                            
+                            Fsm_Valid <= '1';
+                            Fsm_Last  <= '0';
+                            Fsm_Be    <= PlIn_Be;
+                            Fsm_Data  <= PlIn_Data;
+
+                            v.State := Crc_s;
+
+                        -- 2.2) Last data-beat is paritally filled with data
+                        else
+
+                            -- Possible cases:
+                            -- 2.2.1) Entire CRC fits in the remaining space of the last data beat
+                            -- 2.2.2) CRC does not fit in the remaining space of the last data beat.
+                            --        Part of the CRC is combined with DATA in this beat,
+                            --        and the remaining CRC will be appended in subsequent data-beats
+
+                            -- Helper variables
+                            NumBe_v        := count(PlIn_Be, '1');
+                            NumBeWithCrc_v := count(PlIn_Be, '1') + CrcBytes_c;
+
+                            -- 2.2.1) Entire CRC fits in the remaining space of the last data beat
+                            if (CrcBytes_c <= count(PlIn_Be, '0')) then
+
+                                Fsm_Valid <= '1';
+                                Fsm_Last  <= '1';
+                                Fsm_Be    <= 
+                                    zerosVector(BeWidth_c - NumBeWithCrc_v) &
+                                    onesVector(NumBeWithCrc_v);
+                                Fsm_Data  <=
+                                    zerosVector((BeWidth_c - NumBeWithCrc_v)*8) &
+                                    Crc_Crc &
+                                    PlIn_Data(NumBe_v*8-1 downto 0);
+
+                                -- Reset CRC byte counter after the complete 
+                                -- CRC has been appended to the stream.
+                                v.CrcByteCnt := 0;
+
+                                -- Ready to calculate the CRC for the next packet.
+                                Crc_Ready <= '1';
+
+                            -- 2.2.2) CRC does not fit in the remaining space of the last data beat.
+                            --        Part of the CRC is combined with DATA in this beat,
+                            --        and the remaining CRC will be appended in subsequent data-beats
+                            else
+
+                                Fsm_Valid <= '1';
+                                Fsm_Last  <= '0';
+                                Fsm_Be    <= (others => '1');
+                                Fsm_Data  <= 
+                                    Crc_Crc(count(PlIn_Be, '0')*8-1 downto 0)&
+                                    PlIn_Data(NumBe_v*8-1 downto 0);
+
+                                v.CrcByteCnt := count(PlIn_Be, '0');
+                                v.State      := Crc_s;
+
+                            end if;
+                        end if;
+                    end if;
                 end if;
 
+            --------------------------------------------------------------------
             when Crc_s =>
-                Pl_Data                <= (others => '0');
-                Pl_Data(Crc_Crc'range) <= Crc_Crc;
-                Pl_Valid               <= Crc_Valid;
-                Pl_Last                <= '1';
-                Crc_Ready              <= Pl_Ready;
 
-                if Crc_Valid = '1' and Pl_Ready = '1' then
-                    v.State := Data_s;
+                Fsm_Valid <= '1';
+
+                -- Helper Variable
+                CrcBytesRemaining_v := CrcBytes_c - r.CrcByteCnt;
+
+                if (Fsm_Valid = '1' and Fsm_Ready = '1') then
+
+                    -- Possible cases:
+                    --   1) Not the last data beat. CRC bytes are not yet fully appended on the stream
+                    --   2) Last data beat. All CRC bytes are fully appended on the stream
+                        
+                    --   1) Not the last data beat. CRC bytes are not yet fully appended on the stream
+                    if (r.CrcByteCnt + BeWidth_c < CrcBytes_c) then
+
+                        Fsm_Last <= '0';
+                        Fsm_Be   <= (others => '1');
+                        Fsm_Data <= Crc_Crc((r.CrcByteCnt+BeWidth_c)*8-1 downto r.CrcByteCnt*8);
+
+                        v.CrcByteCnt := r.CrcByteCnt + BeWidth_c;
+
+                    --   2) Last data beat. All CRC bytes are fully appended on the stream
+                    else
+
+                        Fsm_Last <= '1';
+                        Fsm_Be   <=
+                            zerosVector(BeWidth_c - CrcBytesRemaining_v) &
+                            onesVector(CrcBytesRemaining_v);
+                        Fsm_Data <=
+                            zerosVector((BeWidth_c - CrcBytesRemaining_v)*8) &
+                            Crc_Crc(CrcBytes_c*8-1 downto r.CrcByteCnt*8);
+
+                        -- Reset CRC byte counter after the complete 
+                        -- CRC has been appended to the stream.
+                        v.CrcByteCnt := 0;
+
+                        -- Ready to calculate the CRC for the next packet.
+                        Crc_Ready <= '1';
+
+                        v.State := Data_s;
+
+                    end if;
                 end if;
 
+            --------------------------------------------------------------------
             -- coverage off
             when others => null; -- unreachable code
             -- coverage on
@@ -148,12 +299,17 @@ begin
 
             -- Reset
             if Rst = '1' then
-                r.State <= Data_s;
+                r.CrcByteCnt <= 0;
+                r.State      <= Data_s;
             end if;
         end if;
     end process;
 
     -- *** Instantiations ***
+
+    ----------------------------------------------------------------------------
+    -- CRC
+    ----------------------------------------------------------------------------
     i_crc : entity work.olo_base_crc
         generic map (
             DataWidth_g     => DataWidth_g,
@@ -165,45 +321,80 @@ begin
             XorOutput_g     => CrcXorOutput_g
         )
         port map (
-            Clk              => Clk,
-            Rst              => Rst,
-            In_Data          => In_Data,
-            In_Valid         => In_Beat,
-            In_Last          => In_Last,
-            Out_Crc          => Crc_Crc,
-            Out_Valid        => Crc_Valid,
-            Out_Ready        => Crc_Ready
+            Clk       => Clk,
+            Rst       => Rst,
+
+            In_Ready  => In_Ready,
+            In_Valid  => In_Valid,
+            In_Last   => In_Last,
+            In_Be     => In_Be,
+            In_Data   => In_Data,
+
+            Out_Ready => Crc_Ready,
+            Out_Valid => Crc_Valid,
+            Out_Crc   => Crc_Crc
         );
 
-    b_pl : block is
-        signal PlInData  : std_logic_vector(DataWidth_g downto 0);
-        signal PlOutData : std_logic_vector(DataWidth_g downto 0);
-    begin
-        -- Input Assembly
-        PlInData <= Pl_Last & Pl_Data;
+    ----------------------------------------------------------------------------
+    -- Input Pipeline Stage
+    --     Required to delay the stream by one clock cycle to allow 
+    --     the CRC component sufficient time to calculate the CRC.
+    ----------------------------------------------------------------------------
+    -- Input Assembly
+    In_Concat <= In_Last & In_Be & In_Data;
 
-        -- Instance
-        i_pl_out : entity work.olo_base_pl_stage
-            generic map (
-                Width_g     => DataWidth_g+1,
-                UseReady_g  => true,
-                Stages_g    => 1
-            )
-            port map (
-                Clk       => Clk,
-                Rst       => Rst,
-                In_Valid  => Pl_Valid,
-                In_Ready  => Pl_Ready,
-                In_Data   => PlInData,
-                Out_Valid => Out_Valid,
-                Out_Ready => Out_Ready,
-                Out_Data  => PlOutData
-            );
+    -- Instance
+    i_pl_in : entity work.olo_base_pl_stage
+        generic map (
+            Width_g     => ConcatWidth_c,
+            UseReady_g  => false,
+            Stages_g    => 1
+        )
+        port map (
+            Clk       => Clk,
+            Rst       => Rst,
 
-        -- Output Assembly
-        Out_Last <= PlOutData(DataWidth_g);
-        Out_Data <= PlOutData(DataWidth_g-1 downto 0);
+            In_Valid  => (In_Valid and In_Ready),
+            In_Data   => In_Concat,
 
-    end block;
+            Out_Valid => PlIn_Valid,
+            Out_Data  => PlIn_Concat
+        );
+
+    -- Output Assembly
+    PlIn_Last <= PlIn_Concat(ConcatWidth_c - 1);
+    PlIn_Be   <= PlIn_Concat(ConcatWidth_c-2 downto DataWidth_g);
+    PlIn_Data <= PlIn_Concat(DataWidth_g-1 downto 0);
+
+    ----------------------------------------------------------------------------
+    -- Output Pipeline Stage
+    ----------------------------------------------------------------------------
+    -- Input Assembly
+    Fsm_Concat <= Fsm_Last & Fsm_Be & Fsm_Data;
+
+    -- Instance
+    i_pl_out : entity work.olo_base_pl_stage
+        generic map (
+            Width_g     => ConcatWidth_c,
+            UseReady_g  => true,
+            Stages_g    => 1
+        )
+        port map (
+            Clk       => Clk,
+            Rst       => Rst,
+
+            In_Ready  => Fsm_Ready,
+            In_Valid  => Fsm_Valid,
+            In_Data   => Fsm_Concat,
+
+            Out_Ready => Out_Ready,
+            Out_Valid => Out_Valid,
+            Out_Data  => PlOut_Concat
+        );
+
+    -- Output Assembly
+    Out_Last <= PlOut_Concat(ConcatWidth_c-1);
+    Out_Be   <= PlOut_Concat(ConcatWidth_c-2 downto DataWidth_g);
+    Out_Data <= PlOut_Concat(DataWidth_g-1 downto 0);
 
 end architecture;
